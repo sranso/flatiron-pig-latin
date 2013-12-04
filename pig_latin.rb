66fdc704c2f0c99af92009147e6e4051f0f34a3a
sranso <sranso@gmail.com>
2013-12-04 12:23:57 -0500

    pig latin can now translate a string into pig latin

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,25 +1,30 @@
-class PigLatin
-  def initialize
-    
-  end
-
-  def word
-    @word
-  end
-
-  def word=(word)
-    @word = word
-  end
-
-  def pig_latin_word
-    split_word = /\A(?<beginning>[yY]?[^aeiouyAEIOUY]*)(?<end>[aeiouyAEIOUY]*[a-zA-Z]*)/.match(@word)
-    if split_word[:beginning] == ""
-      new_word = "#{split_word[:end]}way"
+require "debugger"
+class String
+  def pig_latin
+    if self.include?(" ")
+      words = self.split(/\b/)
+      new_sentence = []
+      words.each do |word|
+        split_word = /\A(?<beginning>[yY]?[^aeiouyAEIOUY]*)(?<end>[aeiouyAEIOUY]*[a-zA-Z]*)/.match(word)
+        if (/\s|\./).match(split_word[:beginning])
+          new_sentence << split_word[:beginning]
+        elsif split_word[:beginning] == ""
+          new_sentence << "#{split_word[:end]}way"
+        else
+          new_sentence << "#{split_word[:end]}#{split_word[:beginning]}ay"
+        end
+      end
+      new_sentence.join
     else
-      new_word = "#{split_word[:end]}#{split_word[:beginning]}ay"
+      split_word = /\A(?<beginning>[yY]?[^aeiouyAEIOUY]*)(?<end>[aeiouyAEIOUY]*[a-zA-Z]*)/.match(self)
+      if split_word[:beginning] == ""
+        new_word = "#{split_word[:end]}way"
+      else
+        new_word = "#{split_word[:end]}#{split_word[:beginning]}ay"
+      end
     end
   end
 end
 
-# banana = PigLatin.new.tap { |obj| obj.word = "banana"}
-# p banana.pig_latin_word
+# has = String.new.tap { |obj| obj.word = "friends"}
+# p has.pig_latin
--- pig_latin_spec.rb
+++ pig_latin_spec.rb
@@ -1,42 +1,39 @@
 require "rspec"
 require "./pig_latin"
 
-describe PigLatin do
-  describe "#pig_latin_word" do
+describe String do
+  describe "#pig_latin" do
     it "For words that begin with consonant sounds, the initial consonant or
      consonant cluster is moved to the end of the word, and ay is added" do
-      banana = PigLatin.new
-      banana.word = "banana"
-      banana.pig_latin_word.should eq("ananabay")
+      "banana".pig_latin.should eq("ananabay")
     end
 
     it "For words that begin with vowel sounds or silent letter, way is added at the
     end of the word." do
-      egg = PigLatin.new
-      egg.word = "egg"
-      egg.pig_latin_word.should eq("eggway")
+      "egg".pig_latin.should eq("eggway")
     end
 
     it "The letter 'y' can play the role of either consonant or vowel, depending
     on its location. Take 1" do
-      yellow = PigLatin.new
-      yellow.word = "yellow"
-      yellow.pig_latin_word.should eq("ellowyay")
+      "yellow".pig_latin.should eq("ellowyay")
     end
 
     it "The letter 'y' can play the role of either consonant or vowel, depending
     on its location. Take 2" do
-      rhythm = PigLatin.new
-      rhythm.word = "rhythm"
-      rhythm.pig_latin_word.should eq("ythmrhay")
+      "rhythm".pig_latin.should eq("ythmrhay")
     end
 
     it "Should account for words that start with double-consonant sounds" do
-      shush = PigLatin.new
-      shush.word = "shush"
-      shush.pig_latin_word.should eq("ushshay")
+      "shush".pig_latin.should eq("ushshay")
     end
 
+  end
+
+  describe '#split_to_words' do
+    it "Should be able to translate an entire sentence into pig latin.
+    THIS IS CRUCIAL" do
+      "Sarah has many friends.".pig_latin.should eq("arahSay ashay anymay iendsfray.")
+    end
   end
   
 end
