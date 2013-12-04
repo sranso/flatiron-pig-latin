bf9d0eff83212e0a1148e539372d8526b87d7580
sranso <sranso@gmail.com>
2013-12-04 12:32:50 -0500

    all done. everything works and is clean

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,28 +1,26 @@
 require "debugger"
+
 class String
   def pig_latin
-    if self.include?(" ")
-      words = self.split(/\b/)
-      new_sentence = []
-      words.each do |word|
-        split_word = /\A(?<beginning>[yY]?[^aeiouyAEIOUY]*)(?<end>[aeiouyAEIOUY]*[a-zA-Z]*)/.match(word)
-        if (/\s|\./).match(split_word[:beginning])
-          new_sentence << split_word[:beginning]
-        elsif split_word[:beginning] == ""
-          new_sentence << "#{split_word[:end]}way"
-        else
-          new_sentence << "#{split_word[:end]}#{split_word[:beginning]}ay"
-        end
-      end
-      new_sentence.join
-    else
-      split_word = /\A(?<beginning>[yY]?[^aeiouyAEIOUY]*)(?<end>[aeiouyAEIOUY]*[a-zA-Z]*)/.match(self)
-      if split_word[:beginning] == ""
-        new_word = "#{split_word[:end]}way"
+    # split sentence into an array by the word breaks
+    all_words = self.split(/\b/)
+    final_sentence = []
+    all_words.each do |word|
+      # divide word based on pig latin's rules
+      split_word = /\A(?<beginning>[yY]?[^aeiouyAEIOUY]*)(?<end>[aeiouyAEIOUY]*[a-zA-Z]*)/.match(word)
+      # if word is a whitespace or a period, don't do anything to it
+      if (/\s|\./).match(split_word[:beginning])
+        final_sentence << split_word[:beginning]
+      # if the word starts with a vowel, :beginning will have captured nothing, and the end gets treated in a special 'way'
+      elsif split_word[:beginning] == ""
+        final_sentence << "#{split_word[:end]}way"
+      # else treat the word the normal piglatin (w)ay
       else
-        new_word = "#{split_word[:end]}#{split_word[:beginning]}ay"
+        final_sentence << "#{split_word[:end]}#{split_word[:beginning]}ay"
       end
     end
+    # join all words and word breaks in the array
+    final_sentence.join
   end
 end
 
